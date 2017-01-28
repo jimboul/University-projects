7c9c4de7d258f953dad11e17516ad9d66e0b8944
jimboul <db.cs@hotmail.com>
2017-01-28 23:55:52 +0200

    Creating a customized version of the well-known Memory Matching Game

---
+++ MemoryMatchingGame/MemoryMatchingGame/Form1.Designer.cs
@@ -0,0 +1,339 @@
+﻿namespace MemoryMatchingGame
+{
+    partial class MemoryMatchingGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.table = new System.Windows.Forms.Panel();
+            this.Card1 = new System.Windows.Forms.PictureBox();
+            this.Card2 = new System.Windows.Forms.PictureBox();
+            this.Card3 = new System.Windows.Forms.PictureBox();
+            this.Card4 = new System.Windows.Forms.PictureBox();
+            this.Card8 = new System.Windows.Forms.PictureBox();
+            this.Card7 = new System.Windows.Forms.PictureBox();
+            this.Card6 = new System.Windows.Forms.PictureBox();
+            this.Card5 = new System.Windows.Forms.PictureBox();
+            this.Card12 = new System.Windows.Forms.PictureBox();
+            this.Card11 = new System.Windows.Forms.PictureBox();
+            this.Card10 = new System.Windows.Forms.PictureBox();
+            this.Card9 = new System.Windows.Forms.PictureBox();
+            this.Card16 = new System.Windows.Forms.PictureBox();
+            this.Card15 = new System.Windows.Forms.PictureBox();
+            this.Card14 = new System.Windows.Forms.PictureBox();
+            this.Card13 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.first_look_timer = new System.Windows.Forms.Timer(this.components);
+            this.countdown_timer = new System.Windows.Forms.Timer(this.components);
+            this.table.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Card1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card16)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card15)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card14)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card13)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // table
+            // 
+            this.table.Controls.Add(this.button1);
+            this.table.Controls.Add(this.label2);
+            this.table.Controls.Add(this.label1);
+            this.table.Controls.Add(this.Card16);
+            this.table.Controls.Add(this.Card15);
+            this.table.Controls.Add(this.Card14);
+            this.table.Controls.Add(this.Card13);
+            this.table.Controls.Add(this.Card12);
+            this.table.Controls.Add(this.Card11);
+            this.table.Controls.Add(this.Card10);
+            this.table.Controls.Add(this.Card9);
+            this.table.Controls.Add(this.Card8);
+            this.table.Controls.Add(this.Card7);
+            this.table.Controls.Add(this.Card6);
+            this.table.Controls.Add(this.Card5);
+            this.table.Controls.Add(this.Card4);
+            this.table.Controls.Add(this.Card3);
+            this.table.Controls.Add(this.Card2);
+            this.table.Controls.Add(this.Card1);
+            this.table.Location = new System.Drawing.Point(13, 13);
+            this.table.Name = "table";
+            this.table.Size = new System.Drawing.Size(764, 649);
+            this.table.TabIndex = 0;
+            // 
+            // Card1
+            // 
+            this.Card1.Image = global::MemoryMatchingGame.Properties.Resources.Card1;
+            this.Card1.Location = new System.Drawing.Point(14, 13);
+            this.Card1.Name = "Card1";
+            this.Card1.Size = new System.Drawing.Size(90, 135);
+            this.Card1.TabIndex = 0;
+            this.Card1.TabStop = false;
+            // 
+            // Card2
+            // 
+            this.Card2.Image = global::MemoryMatchingGame.Properties.Resources.Card1;
+            this.Card2.Location = new System.Drawing.Point(121, 13);
+            this.Card2.Name = "Card2";
+            this.Card2.Size = new System.Drawing.Size(90, 135);
+            this.Card2.TabIndex = 1;
+            this.Card2.TabStop = false;
+            // 
+            // Card3
+            // 
+            this.Card3.Image = global::MemoryMatchingGame.Properties.Resources.Card2;
+            this.Card3.Location = new System.Drawing.Point(228, 13);
+            this.Card3.Name = "Card3";
+            this.Card3.Size = new System.Drawing.Size(90, 135);
+            this.Card3.TabIndex = 2;
+            this.Card3.TabStop = false;
+            // 
+            // Card4
+            // 
+            this.Card4.Image = global::MemoryMatchingGame.Properties.Resources.Card2;
+            this.Card4.Location = new System.Drawing.Point(337, 13);
+            this.Card4.Name = "Card4";
+            this.Card4.Size = new System.Drawing.Size(90, 135);
+            this.Card4.TabIndex = 3;
+            this.Card4.TabStop = false;
+            // 
+            // Card8
+            // 
+            this.Card8.Image = global::MemoryMatchingGame.Properties.Resources.Card4;
+            this.Card8.Location = new System.Drawing.Point(337, 163);
+            this.Card8.Name = "Card8";
+            this.Card8.Size = new System.Drawing.Size(90, 135);
+            this.Card8.TabIndex = 7;
+            this.Card8.TabStop = false;
+            // 
+            // Card7
+            // 
+            this.Card7.Image = global::MemoryMatchingGame.Properties.Resources.Card4;
+            this.Card7.Location = new System.Drawing.Point(228, 163);
+            this.Card7.Name = "Card7";
+            this.Card7.Size = new System.Drawing.Size(90, 135);
+            this.Card7.TabIndex = 6;
+            this.Card7.TabStop = false;
+            // 
+            // Card6
+            // 
+            this.Card6.Image = global::MemoryMatchingGame.Properties.Resources.Card3;
+            this.Card6.Location = new System.Drawing.Point(121, 163);
+            this.Card6.Name = "Card6";
+            this.Card6.Size = new System.Drawing.Size(90, 135);
+            this.Card6.TabIndex = 5;
+            this.Card6.TabStop = false;
+            // 
+            // Card5
+            // 
+            this.Card5.Image = global::MemoryMatchingGame.Properties.Resources.Card3;
+            this.Card5.Location = new System.Drawing.Point(14, 163);
+            this.Card5.Name = "Card5";
+            this.Card5.Size = new System.Drawing.Size(90, 135);
+            this.Card5.TabIndex = 4;
+            this.Card5.TabStop = false;
+            // 
+            // Card12
+            // 
+            this.Card12.Image = global::MemoryMatchingGame.Properties.Resources.Card6;
+            this.Card12.Location = new System.Drawing.Point(337, 313);
+            this.Card12.Name = "Card12";
+            this.Card12.Size = new System.Drawing.Size(90, 135);
+            this.Card12.TabIndex = 11;
+            this.Card12.TabStop = false;
+            // 
+            // Card11
+            // 
+            this.Card11.Image = global::MemoryMatchingGame.Properties.Resources.Card6;
+            this.Card11.Location = new System.Drawing.Point(228, 313);
+            this.Card11.Name = "Card11";
+            this.Card11.Size = new System.Drawing.Size(90, 135);
+            this.Card11.TabIndex = 10;
+            this.Card11.TabStop = false;
+            // 
+            // Card10
+            // 
+            this.Card10.Image = global::MemoryMatchingGame.Properties.Resources.Card5;
+            this.Card10.Location = new System.Drawing.Point(121, 313);
+            this.Card10.Name = "Card10";
+            this.Card10.Size = new System.Drawing.Size(90, 135);
+            this.Card10.TabIndex = 9;
+            this.Card10.TabStop = false;
+            // 
+            // Card9
+            // 
+            this.Card9.Image = global::MemoryMatchingGame.Properties.Resources.Card5;
+            this.Card9.Location = new System.Drawing.Point(14, 313);
+            this.Card9.Name = "Card9";
+            this.Card9.Size = new System.Drawing.Size(90, 135);
+            this.Card9.TabIndex = 8;
+            this.Card9.TabStop = false;
+            // 
+            // Card16
+            // 
+            this.Card16.Image = global::MemoryMatchingGame.Properties.Resources.Card8;
+            this.Card16.Location = new System.Drawing.Point(337, 466);
+            this.Card16.Name = "Card16";
+            this.Card16.Size = new System.Drawing.Size(90, 135);
+            this.Card16.TabIndex = 15;
+            this.Card16.TabStop = false;
+            // 
+            // Card15
+            // 
+            this.Card15.Image = global::MemoryMatchingGame.Properties.Resources.Card8;
+            this.Card15.Location = new System.Drawing.Point(228, 466);
+            this.Card15.Name = "Card15";
+            this.Card15.Size = new System.Drawing.Size(90, 135);
+            this.Card15.TabIndex = 14;
+            this.Card15.TabStop = false;
+            // 
+            // Card14
+            // 
+            this.Card14.Image = global::MemoryMatchingGame.Properties.Resources.Card7;
+            this.Card14.Location = new System.Drawing.Point(121, 466);
+            this.Card14.Name = "Card14";
+            this.Card14.Size = new System.Drawing.Size(90, 135);
+            this.Card14.TabIndex = 13;
+            this.Card14.TabStop = false;
+            // 
+            // Card13
+            // 
+            this.Card13.Image = global::MemoryMatchingGame.Properties.Resources.Card7;
+            this.Card13.Location = new System.Drawing.Point(14, 466);
+            this.Card13.Name = "Card13";
+            this.Card13.Size = new System.Drawing.Size(90, 135);
+            this.Card13.TabIndex = 12;
+            this.Card13.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cambria Math", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(456, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 89);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Score: 0 points";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cambria Math", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(456, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(174, 89);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Time: 45 seconds";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(522, 498);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 61);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "Play Again!";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // first_look_timer
+            // 
+            this.first_look_timer.Interval = 2000;
+            // 
+            // countdown_timer
+            // 
+            this.countdown_timer.Interval = 1000;
+            this.countdown_timer.Tick += new System.EventHandler(this.countdown_timer_Tick);
+            // 
+            // MemoryMatchingGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(877, 674);
+            this.Controls.Add(this.table);
+            this.Name = "MemoryMatchingGame";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MemoryMatchingGame_Load);
+            this.table.ResumeLayout(false);
+            this.table.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Card1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card16)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card15)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card14)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Card13)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel table;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox Card16;
+        private System.Windows.Forms.PictureBox Card15;
+        private System.Windows.Forms.PictureBox Card14;
+        private System.Windows.Forms.PictureBox Card13;
+        private System.Windows.Forms.PictureBox Card12;
+        private System.Windows.Forms.PictureBox Card11;
+        private System.Windows.Forms.PictureBox Card10;
+        private System.Windows.Forms.PictureBox Card9;
+        private System.Windows.Forms.PictureBox Card8;
+        private System.Windows.Forms.PictureBox Card7;
+        private System.Windows.Forms.PictureBox Card6;
+        private System.Windows.Forms.PictureBox Card5;
+        private System.Windows.Forms.PictureBox Card4;
+        private System.Windows.Forms.PictureBox Card3;
+        private System.Windows.Forms.PictureBox Card2;
+        private System.Windows.Forms.PictureBox Card1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer first_look_timer;
+        private System.Windows.Forms.Timer countdown_timer;
+    }
+}
+
---
+++ MemoryMatchingGame/MemoryMatchingGame/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MemoryMatchingGame
+{
+    public partial class MemoryMatchingGame : Form
+    {
+
+        int attempt_counter = 0; //Δημιουργώ ένα μετρητή προσπαθειών
+        int suc_att_c = 0; //Δημιουργώ έναν μετρητή επιτυχημένων προσπαθειών
+        public int sec_counter = 45;//Δημιουργώ τον μετρητή για την αντίστροφη μέτρηση μέχρι το τέλος του παιχνιδιού
+        int score = 0;//Κρατάει το score του παιχνιδιού
+        string result; // Δείχνει το αποτέλεσμα του παιχνιδιού
+        Random rand = new Random(); // Διασφαλίζουμε την τυχαιότητα στην κατανομή των καρτών
+
+
+        public MemoryMatchingGame()
+        {
+            InitializeComponent();
+        }
+
+        private void MemoryMatchingGame_Load(object sender, EventArgs e)
+        {
+            foreach (PictureBox picbox in table.Controls)
+            {
+                picbox.Image = Properties.Resources.Cover;
+            }
+        }
+
+        private void countdown_timer_Tick(object sender, EventArgs e)
+        {
+            sec_counter--;
+            label2.Text = "Time: " + sec_counter + " seconds";
+            if (sec_counter == 0)
+            {
+                countdown_timer.Stop();
+                result = "Loser (out of time)";
+                //scores.WriteLine(result);
+                //scores.WriteLine(score + " points");
+                //scores.WriteLine(45 - sec_counter + " sec");
+                //scores.WriteLine(attempt_counter + " attempts. " + suc_att_c + " successful and " + (attempt_counter - suc_att_c) + " unsuccessful.");
+                //scores.Close();
+                //player1.Play();
+                MessageBox.Show("Time is over! You lost! You need some more practice!");
+                Application.Exit();
+                Close();
+            }
+        }
+    }
+}
